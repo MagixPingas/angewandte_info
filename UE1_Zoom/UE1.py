050d9128409a550b4768c27af7d1342cbacc671a
Gernet, Marco <marco.gernet@study.thws.de>
2025-10-30 18:29:24 +0100

    Kommentar in UE1.py hinzugefügt

--- UE1_Zoom/UE1.py
+++ UE1_Zoom/UE1.py
@@ -7,9 +7,9 @@ class MainWindow(QMainWindow):
         uic.loadUi('ue1_widget.ui', self)
         self.resize(800, 600)
         self.setWindowTitle("UE1:Zoom")
-        self.bigButton.clicked.connect(lambda: self.button_click(1.1))
-        self.smallButton.clicked.connect(lambda: self.button_click(0.9))
-        self.show()
+        self.bigButton.clicked.connect(lambda: self.button_click(1.1))      #Damit dann etwas geschieht, muss das Signal der Komponente
+        self.smallButton.clicked.connect(lambda: self.button_click(0.9))    #mit einer auszuführenden Funktion/Methode ("Slot") verknuepft werden.
+        self.show()                                                         #slides_02_Qt5_basics_handout.pdf Folie 2
 
 
     def button_click(self, factor):
